711b33741beab35723f6f5255e6344398bfddf14
Nivall <nivahmichael@gmail.com>
2025-11-13 20:20:46 +0300

    updated the URL path

--- app/src/main/java/com/example/Varsani/utils/Urls.java
+++ app/src/main/java/com/example/Varsani/utils/Urls.java
@@ -3,7 +3,7 @@ package com.example.Varsani.utils;
 public class Urls {
 
 
-   //public static String ipAddress = "https://1b557a947286.ngrok-free.app/redbubble/";
+   //public static String ipAddress = "https://1b557a94728.ngrok-free.app/redbubble/";
     public static String ipAddress = "http://192.168.100.215/redbubble/";
 
     private static final String ROOT_URL =ipAddress+ "android_files/";
